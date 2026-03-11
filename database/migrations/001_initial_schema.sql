5de9e477ddc8268f21991456c45bb7e7ea211c13
Frank Willeke <frank@frankwilleke.de>
2026-03-11 09:59:19 +0100

    Added SQL setup files

---
+++ database/migrations/001_initial_schema.sql
@@ -0,0 +1,3 @@
+-- Initial schema
+
+SOURCE ../schema.sql;
---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+-- Optional starter data for Pulse
+
+SET NAMES utf8mb4;
+
+-- Example settings keys
+-- mail.from_address
+-- mail.from_name
+-- app.base_url
