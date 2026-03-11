1049a76b566eb1ce6850e4fc17211fc63939e6c2
Frank Willeke <frank@frankwilleke.de>
2026-03-11 14:57:05 +0100

    Code cleanup

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -5,23 +5,15 @@ declare(strict_types=1);
 ob_start();
 ?>
 <div class="card">
-
 	<?php if (is_array($flash)): ?>
 		<div class="flash flash-<?= htmlspecialchars((string)$flash['type'], ENT_QUOTES, 'UTF-8') ?>">
 			<?= htmlspecialchars((string)$flash['message'], ENT_QUOTES, 'UTF-8') ?>
 		</div>
 	<?php endif; ?>
 
-	<p>
-		Welcome<?= is_array($user) ? ', ' . htmlspecialchars((string)$user['display_name'], ENT_QUOTES, 'UTF-8') : '' ?>.
-	</p>
+	<p>Welcome<?= is_array($user) ? ', ' . htmlspecialchars((string)$user['display_name'], ENT_QUOTES, 'UTF-8') : '' ?>.</p>
 
 	<p>This is the initial Pulse dashboard.</p>
-
-	<p>
-		<a href="/logout">Log out</a>
-	</p>
-
 </div>
 
 <?php
