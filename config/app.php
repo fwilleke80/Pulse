5c8928be33950444f583ebe653a58e1f1b3d3640
Frank Willeke <frank@frankwilleke.de>
2026-03-12 22:31:37 +0100

    Increased version number to 0.2.4

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
 	'name' => 'Pulse',
-	'version' => '0.2.0',
+	'version' => '0.2.4',
 	'env' => 'production',
 	'debug' => true,
 	'base_url' => 'https://pulse.frankwilleke.de',
