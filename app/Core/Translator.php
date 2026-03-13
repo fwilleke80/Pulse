3825ea5565afcf155b5b960b532993012ba64d91
Frank Willeke <frank@frankwilleke.de>
2026-03-13 22:56:37 +0100

    Added documentation

--- app/Core/Translator.php
+++ app/Core/Translator.php
@@ -6,11 +6,18 @@ namespace Pulse\Core;
 
 /**
  * @brief Simple translation service.
+ * Loads the appropriate language file based on the locale and provides a method to translate keys to their corresponding strings.
  */
 class Translator
 {
 	private array $_strings;
 
+	/**
+	 * @brief Constructs the translator by loading the appropriate language file.
+	 * @param string $langPath Path to the language files.
+	 * @param string $locale Locale to load (e.g. "en", "de").
+	 * @throws \RuntimeException If the language file cannot be found.
+	 */
 	public function __construct(string $langPath, string $locale)
 	{
 		$file = $langPath . '/' . $locale . '.php';
